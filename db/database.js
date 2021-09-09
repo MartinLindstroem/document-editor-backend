73dcfe697ac22144a76b8044bc8115caf6e26834
Martin Lindström <martinllindstroem@gmail.com>
2021-09-09 12:10:21 +0200

    created database object to handle db connection

--- db/database.js
+++ db/database.js
@@ -1,1 +1,27 @@
-'mongodb+srv://texteditor:<password>@cluster0.2kqjd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongo = require("mongodb").MongoClient;
+const config = require("./config.json");
+const collectionName = "docs";
+
+const database = {
+    getDb: async function getDb() {
+        let dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.2kqjd.mongodb.net/jsramverk?retryWrites=true&w=majority`;
+
+        if (process.env.NODE_ENV === 'test') {
+            dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.2kqjd.mongodb.net/test?retryWrites=true&w=majority`;
+        }
+
+        const client = await mongo.connect(dsn, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = await client.db();
+        const collection = await db.collection(collectionName);
+
+        return {
+            collection: collection,
+            client: client,
+        };
+    }
+};
+
+module.exports = database;
