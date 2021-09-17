732a432e554ebf1172aabedc7aa3837c757ffbd5
Martin Lindström <martinllindstroem@gmail.com>
2021-09-17 11:06:25 +0200

    added methods for inserting, updating and getting documents from db

--- db/database.js
+++ db/database.js
@@ -1,13 +1,17 @@
 const mongo = require("mongodb").MongoClient;
 const config = require("./config.json");
 const collectionName = "docs";
+const objectId = require('mongodb').ObjectId;
 
 const database = {
     getDb: async function getDb() {
-        let dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.2kqjd.mongodb.net/jsramverk?retryWrites=true&w=majority`;
+        let dsn = `mongodb+srv://${config.username}:${config.password}
+@cluster0.2kqjd.mongodb.net/jsramverk?retryWrites=true&w=majority`;
 
         if (process.env.NODE_ENV === 'test') {
-            dsn = `mongodb+srv://${config.username}:${config.password}@cluster0.2kqjd.mongodb.net/test?retryWrites=true&w=majority`;
+            // dsn = `mongodb+srv://${config.username}:${config.password}
+            // @cluster0.2kqjd.mongodb.net/test?retryWrites=true&w=majority`;
+            dsn = "mongodb://localhost:27017/test";
         }
 
         const client = await mongo.connect(dsn, {
@@ -18,9 +22,62 @@ const database = {
         const collection = await db.collection(collectionName);
 
         return {
+            db: db,
             collection: collection,
             client: client,
         };
+    },
+    insertDocument: async function insertDoc(name, content) {
+        const data = {
+            name: name,
+            content: content,
+        };
+
+        const db = await this.getDb();
+
+        const result = await db.collection.insertOne(data);
+
+        await db.client.close();
+
+        if (result.acknowledged) {
+            // itemId = result.insertedId;
+            return {
+                _id: result.insertedId,
+                name: data.name,
+                content: data.content
+            };
+        }
+    },
+
+    updateDocument: async function updateDoc(id, name, content) {
+        const filter = { _id: objectId(id) };
+        const data = {
+            name: name,
+            content: content,
+        };
+
+        const db = await this.getDb();
+        const result = await db.collection.updateOne(filter, { $set: data });
+
+        await db.client.close();
+
+        if (result.acknowledged) {
+            return {
+                _id: result.upsertedId,
+                name: data.name,
+                content: data.content
+            };
+        }
+    },
+
+    getAllDocuments: async function getAllDocs() {
+        const db = await this.getDb();
+        const col = db.collection;
+        const res = await col.find().toArray();
+
+        await db.client.close();
+
+        return res;
     }
 };
 
