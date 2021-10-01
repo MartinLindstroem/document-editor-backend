c0ee389e962fe69bea0b9917d1ac04ffe818d58a
Martin Lindström <martinllindstroem@gmail.com>
2021-10-01 17:20:47 +0200

    updated route

--- routes/insert.js
+++ routes/insert.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const database = require("../db/database");
+const auth = require("../src/auth");
 
-router.post('/', async function (req, res) {
-    const data = await database.insertDocument(req.body.name, req.body.content);
+// router.post('/', async function (req, res) {
+//     const data = await database.insertDocument("docs", req.body.name, req.body.content);
 
-    return res.status(201).json({ data });
-});
+//     return res.status(201).json({ data });
+// });
+router.post("/",
+    // (req, res, next) => auth.checkToken(req, res, next),
+    (req, res) => database.insertDocument(res, "docs", req.body));
 
 module.exports = router;
--- routes/update.js
+++ routes/update.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const database = require("../db/database");
+const auth = require("../src/auth");
 // const objectId = require('mongodb').ObjectId;
 
-router.put('/', async function (req, res) {
-    const data = await database.updateDocument(req.body._id, req.body.name, req.body.content);
+router.put('/',
+    // (req, res, next) => auth.checkToken(req, res, next),
+    (req, res) => database.updateDocument(res, "docs", req.body));
+// const data = await database.updateDocument
+//("docs", req.body._id, req.body.name, req.body.content);
 
-    return res.status(200).json({ data });
-});
+// return res.status(200).json({ data });
 
 module.exports = router;
